13810c475db7b67b6b09d7327ea4d1613159871a
Amirkhan <dddddsasq@gmail.com>
2025-08-08 22:47:13 +0500

    First commit

---
+++ src/main/java/com/example/getContact/GetContactApplication.java
@@ -0,0 +1,14 @@
+package com.example.getContact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetContactApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Amirkhan ");
+		SpringApplication.run(GetContactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getContact/GetContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getContact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
